d4141d527524ab1617ccf2a72167816a6078a631
skyend74-dot <skyend74@gmail.com>
2025-09-27 17:28:37 +0500

    Create http.ts

---
+++ api/lots/lib/http.ts
@@ -0,0 +1,12 @@
+export function jsonOK(data: any, init: number | ResponseInit = 200) {
+  return new Response(JSON.stringify({ ok: true, ...data }), {
+    status: typeof init === 'number' ? init : init.status ?? 200,
+    headers: { 'content-type': 'application/json; charset=utf-8' },
+  });
+}
+export function jsonErr(message: string, status = 400) {
+  return new Response(JSON.stringify({ ok: false, error: message }), {
+    status,
+    headers: { 'content-type': 'application/json; charset=utf-8' },
+  });
+}
